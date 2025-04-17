7c9f487061d60dec204d768dd7cfd97f3acac343
Ian Reis <ian.reis@hey.com>
2025-04-17 12:44:05 -0400

    refactor: restructure project and update configs
    - Move server.ts to index.ts
    
    - Add DVMCP configs
    
    - Update package dependencies
    
    - Clean up unused files
    
    - Add assets

---
+++ packages/promo-extension/ROADMAP.md
@@ -0,0 +1,3 @@
+# ROADMAP
+- [ ] key signer
+- [ ] listen for refresh events and display notification
--- packages/promo-server/src/handler.ts
+++ packages/promo-server/src/handler.ts
@@ -1,23 +1,47 @@
-  import { get_metrics } from "./lib/get-metrics";
+import { get_metrics } from "./lib/get-metrics";
 import { publish_refresh_event } from "./lib/publish-refresh-event";
-import type { Logger } from 'pino';
 import type { RelayHandler } from "@dvmcp/commons/nostr/relay-handler";
 import type { KeyManager } from "@dvmcp/commons/nostr/key-manager";
+import type { Logger } from 'pino';
+import { ToolRegistry } from "@dvmcp/discovery/src/tool-registry";
+import { ToolExecutor } from "@dvmcp/discovery/src/tool-executor";
+
+/**
+ * This function is the main handler for the billboard.
+ * It gets the metrics from the database and publishes a refresh event to the billboard.
+ * @param param0 
+ * @param param1 
+ */
+export const handler = async ({
+  since,
+  until,
+}: HandlerParams,
+  {
+    tool_registry,
+    key_manager,
+    relay_handler,
+    logger
+  }: HandlerDependencies
+): Promise<void> => {
+
+  const tool_executor = new ToolExecutor(relay_handler, key_manager, tool_registry);
+  logger.debug({ since, until });
+  logger.trace('getting metrics');
+  const metrics = await get_metrics({ since, until }, { tool_executor, logger });
+  logger.debug({ metrics }, 'metrics');
+  logger.trace('publishing refresh event');
+  await publish_refresh_event({ metrics }, { key_manager, relay_handler, logger });
+  logger.trace('refresh event published');
+};
 
 export type HandlerParams = {
+  since: number;
+  until: number;
 }
 
 export type HandlerDependencies = {
+  tool_registry: ToolRegistry;
   relay_handler: RelayHandler;
   logger: Logger;
   key_manager: KeyManager;
 }
-
-export const handler = async ({ }: HandlerParams, { key_manager, relay_handler, logger }: HandlerDependencies) => {
-  const since = Math.floor(Date.now() / 1000) - 60;
-  const until = Math.floor(Date.now() / 1000);
-  logger.debug({ since, until });
-  const metrics = await get_metrics({ since, until }, { key_manager, relay_handler, logger });
-  logger.debug({ metrics }, 'metrics');
-  await publish_refresh_event({ metrics }, { key_manager, relay_handler, logger });
-};
